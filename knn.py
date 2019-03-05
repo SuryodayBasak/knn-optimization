95c5da9f138f98b98e2db1e6ddbd3a6e486a2771
Suryoday Basak <suryodaybasak@gmail.com>
2019-03-05 03:41:58 -0600

    Added distance weighted knn

--- knn.py
+++ knn.py
@@ -23,9 +23,9 @@ class DirectionBalancedKNN:
                     d_l.append(np.sqrt((x-self.X[i])**2)) #Euclidean dist
 
                 else:
-                    x_l.append(self.X[i])
-                    y_l.append(self.y[i])
-                    d_l.append(np.sqrt((x-self.X[i])**2)) #Euclidean dist
+                    x_r.append(self.X[i])
+                    y_r.append(self.y[i])
+                    d_r.append(np.sqrt((x-self.X[i])**2)) #Euclidean dist
 
             sorted_l_idx = np.argsort(d_l)
             sorted_r_idx = np.argsort(d_r)
@@ -40,6 +40,64 @@ class DirectionBalancedKNN:
             for j in r_idx:
                 y += y_r[j]
 
-            y = y/self.k
+            y = y/(self.k*2)
             y_pred.append(y)
         return y_pred
+
+class InvDistDirectionBalancedKNN:
+    def __init__(self, X, y, k):
+        self.X = X
+        self.y = y
+        self.k = k
+
+    def predict(self, X_test):
+        y_pred = []
+        for x in X_test:
+            x_l = []
+            y_l = []
+            x_r = []
+            y_r = []
+            d_l = []
+            d_r = []
+
+            for i in range(len(self.X)):
+                if (self.X[i] < x):
+                    x_l.append(self.X[i])
+                    y_l.append(self.y[i])
+                    d_l.append(np.sqrt((x-self.X[i])**2)) #Euclidean dist
+
+                else:
+                    x_r.append(self.X[i])
+                    y_r.append(self.y[i])
+                    d_r.append(np.sqrt((x-self.X[i])**2)) #Euclidean dist
+
+            sorted_l_idx = np.argsort(d_l)
+            sorted_r_idx = np.argsort(d_r)
+
+            l_idx = sorted_l_idx[:self.k]
+            r_idx = sorted_r_idx[:self.k]
+
+            y = 0.0
+            w_sum = 0.0
+
+            for j in l_idx:
+                y += (1/d_l[j])*y_l[j]
+                w_sum += (1/d_l[j])
+
+            for j in r_idx:
+                y += (1/d_r[j])*y_r[j]
+                w_sum += (1/d_r[j])
+
+            y = y/(w_sum)
+            y_pred.append(y)
+        return y_pred
+
+class WeightOptimizedKNN:
+    def __init__(self, X, y, k):
+        self.X = X
+        self.y = y
+        self.k = k
+
+    def train(self):
+        for i in range(len(self.X)):
+            print(self.X[i])
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from data_gen import CosineData
-from knn import DirectionBalancedKNN
+from knn import DirectionBalancedKNN, InvDistDirectionBalancedKNN, WeightOptimizedKNN
 
 data_obj = CosineData(0, np.pi/2 + 0.5,
             [0, np.pi/8, np.pi/4+0.1, np.pi/2+0.2, np.pi/2 + 1.5],
-            [0.08, 0.08, 0.08, 0.08, 0.1], 0.0)
+            [0.08, 0.2, 0.2, 0.2, 0.1], 0.0)
 data_obj.generate()
 X,y = data_obj.get_rn()
 plt.scatter(X, y)
@@ -14,9 +14,21 @@ test_data_x = np.linspace(0.01, np.pi/2 + 1.4, 100)
 test_data_y = np.cos(test_data_x)
 
 plt.plot(test_data_x, test_data_y)
-reg = DirectionBalancedKNN(X, y, 5)
 
+#Method 1
+reg = DirectionBalancedKNN(X, y, 2)
 test_data_pred = reg.predict(test_data_x)
-print(test_data_pred)
 plt.plot(test_data_x, test_data_pred)
+
+#Method 2
+reg = InvDistDirectionBalancedKNN(X, y, 2)
+test_data_pred = reg.predict(test_data_x)
+plt.plot(test_data_x, test_data_pred)
+
 plt.show()
+
+#Method 3
+#reg = DirectionBalancedKNN(X, y, 2)
+#test_data_pred = reg.predict(test_data_x)
+#plt.plot(test_data_x, test_data_pred)
+
