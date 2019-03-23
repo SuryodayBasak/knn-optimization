1603347cd72dbe3863206b60e6900841154ca6bb
Suryoday Basak <suryodaybasak@gmail.com>
2019-03-23 03:32:31 -0600

    Adding noise stubs

---
+++ continuous_ngbh/data_gen.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+f_type = ['parabola', 'line', 'asymmetric parabola']
+n_type = ['white', 'flicker', 'pink', 'red', 'blue', 'violet']
+#incorporate grey noise later
+data_dist = ['uniform', 'gaussian', 'exponential', 'piecewise']
+
+
---
+++ continuous_ngbh/noise.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def white(x, y, var):
+    pass
+
+def pink(x, y, var):
+    pass
+
+def red(x, y, var):
+    pass
+
+def blue(x, y, var):
+    pass
+
+def violet(x, y, var):
+    pass
+
+
