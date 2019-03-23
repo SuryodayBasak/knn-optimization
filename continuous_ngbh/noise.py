32eb3ef816f912d3e75f60acb2c6cd318446f233
Suryoday Basak <suryodaybasak@gmail.com>
2019-03-23 03:39:19 -0600

    Adding functions for basic noise simulation

--- continuous_ngbh/noise.py
+++ continuous_ngbh/noise.py
@@ -1,18 +1,22 @@
 import numpy as np
+import scipy
 
 def white(x, y, var):
-    pass
+    noise = np.random.normal(0, var)
+    return y + noise
 
 def pink(x, y, var):
-    pass
+    noise = np.random.normal(0, var/x)
+    return y + noise
 
 def red(x, y, var):
-    pass
+    noise = np.random.normal(0, var/(x**2))
+    return y + noise
 
 def blue(x, y, var):
-    pass
+    noise = np.random.normal(0, var*x)
+    return y + noise
 
 def violet(x, y, var):
-    pass
-
-
+    noise = np.random.normal(0, var*(x**2))
+    return y + noise
