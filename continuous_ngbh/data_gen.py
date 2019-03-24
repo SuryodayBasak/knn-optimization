28090361393fa2d93639d5b0483a7d027098982e
Suryoday Basak <suryodaybasak@gmail.com>
2019-03-24 02:28:26 -0500

    Including linear and quadratic data generation functions

--- continuous_ngbh/data_gen.py
+++ continuous_ngbh/data_gen.py
@@ -5,4 +5,21 @@ n_type = ['white', 'flicker', 'pink', 'red', 'blue', 'violet']
 #incorporate grey noise later
 data_dist = ['uniform', 'gaussian', 'exponential', 'piecewise']
 
+def linear_func(X):
+    m = 1
+    c = 1
+    y = [m*x + c for x in X]
+    return y
+
+def quadratic_func(X):
+    y = [-(x-2)**2 + 1 for x in X]
+    return y
+
+#Generate data between 1 and 3
+
+#Generate linear data
+
+
+
+#Generate quadratic data
 
